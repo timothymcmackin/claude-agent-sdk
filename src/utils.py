46af1eb6c8dedddec9a39dfdb74ae217ac0737f7
Tim McMackin <timothymcmackin@yahoo.com>
2026-06-10 19:23:48 -0400

    Claude corrected file

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 def calculate_average(numbers):
+    if not numbers:
+        return 0
     total = 0
     for num in numbers:
         total += num
@@ -6,4 +8,9 @@ def calculate_average(numbers):
 
 
 def get_user_name(user):
-    return user["name"].upper()
+    if not user:
+        return ""
+    name = user.get("name")
+    if not name:
+        return ""
+    return str(name).upper()
